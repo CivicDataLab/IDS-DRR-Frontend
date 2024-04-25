e4358d741a54c6abdab4559faac01151367544c8
ruthvik129 <ruthvik129@gmail.com>
2024-04-25 12:37:32 +0530

    update filter component functionality

--- app/[locale]/analytics/components/filter-comp.tsx
+++ app/[locale]/analytics/components/filter-comp.tsx
@@ -1,12 +1,7 @@
-import { text } from 'stream/consumers';
 import React, { useState } from 'react';
-import { type TypedDocumentNode } from '@graphql-typed-document-node/core';
+import { parseDate } from '@internationalized/date';
 import { useQuery } from '@tanstack/react-query';
-import {
-  parseAsArrayOf,
-  parseAsString,
-  useQueryState,
-} from 'next-usequerystate';
+import { parseAsString, useQueryState } from 'next-usequerystate';
 import {
   Button,
   Drawer,
@@ -18,18 +13,19 @@ import {
   DrawerTitle,
   Icon,
   IconButton,
+  MonthPicker,
   RadioGroup,
   RadioItem,
   Text,
+  TextField,
 } from 'opub-ui';
 
 import {
-  ANALYTICS_DISTRICT_MAP_DATA,
   ANALYTICS_GEOGRAPHY_DATA,
-  ANALYTICS_REVENUE_MAP_DATA,
   ANALYTICS_TIME_PERIODS,
 } from '@/config/graphql/analaytics-queries';
 import { GraphQL } from '@/lib/api';
+import { cn, formatDate } from '@/lib/utils';
 import Icons from '@/components/icons';
 
 export function FilterComp({
@@ -48,47 +44,38 @@ export function FilterComp({
 
   const [isDrawerOpen, setIsDrawerOpen] = useState(false);
 
+  const [selectedOption, setSelectedOption] = useState('boundary');
+
+  const [regionOptions, setRegionOptions] = useState<Option[] | never[]>([]);
+
   const [boundary, setBoundary] = useQueryState(
     'boundary',
     parseAsString.withDefault('district')
   );
 
-  const [timePeriodSelected, setTimePeriod] = useQueryState(
+  const [, setTimePeriod] = useQueryState(
     'time-period',
     parseAsString.withDefault(timePeriod)
   );
 
-  const [region, setRegion] = useQueryState(
-    'region',
-    parseAsArrayOf(parseAsString)
-  );
+  const [region, setRegion] = useQueryState('region');
 
-  const [selectedGroup, setSelectedGroup] = React.useState<string[]>([]);
-  const mapQuery: TypedDocumentNode<any, any> =
-    boundary === 'district'
-      ? ANALYTICS_DISTRICT_MAP_DATA
-      : ANALYTICS_REVENUE_MAP_DATA;
+  const [boundarySelected, setBoundarySelected] = useState(boundary);
 
-  const mapData = useQuery(
-    [`mapQuery_${boundary}_${indicator}_${timePeriodSelected}`],
-    () =>
-      GraphQL('analytics', mapQuery, {
-        indcFilter: { slug: indicator },
-        dataFilter: { dataPeriod: timePeriodSelected },
-      }),
-    {
-      refetchOnMount: false,
-      refetchOnWindowFocus: false,
-      refetchOnReconnect: false,
-    }
-  );
+  const [regionSelected, setRegionSelected] = useState('');
+
+  const [timePeriodSelected, setTimePeriodSelected] = useState(timePeriod);
 
   const geographiesData = useQuery(
-    [`geographies_data_${boundary}`],
+    [`geographies_data_${boundarySelected}`],
     () =>
-      GraphQL('analytics', ANALYTICS_GEOGRAPHY_DATA, {
-        geoFilter: { type: boundary },
-      }),
+      GraphQL(
+        `${process.env.NEXT_PUBLIC_DATA_MANAGEMENT_LAYER_URL}/graphql`,
+        ANALYTICS_GEOGRAPHY_DATA,
+        {
+          geoFilter: { type: boundarySelected },
+        }
+      ),
     {
       refetchOnMount: false,
       refetchOnWindowFocus: false,
@@ -98,17 +85,22 @@ export function FilterComp({
 
   const timePeriods = useQuery(
     [`timePeriods`],
-    () => GraphQL('analytics', ANALYTICS_TIME_PERIODS),
+    () =>
+      GraphQL(
+        `${process.env.NEXT_PUBLIC_DATA_MANAGEMENT_LAYER_URL}/graphql`,
+        ANALYTICS_TIME_PERIODS
+      ),
     {
       refetchOnMount: false,
       refetchOnWindowFocus: false,
       refetchOnReconnect: false,
     }
   );
-  let RevCircleDropdownOptions: Option[] = [];
-  let DistrictDropDownOption: Option[] = [];
-  if (geographiesData.data && !geographiesData.isFetching) {
-    if (boundary === 'revenue-circle') {
+
+  const getRegionOptions = React.useCallback(() => {
+    let RevCircleDropdownOptions: Option[] = [];
+    let DistrictDropDownOption: Option[] = [];
+    if (boundarySelected === 'revenue-circle') {
       let rawData = geographiesData?.data?.getDistrictRevCircle;
       if (rawData) {
         for (const district in rawData) {
@@ -118,112 +110,74 @@ export function FilterComp({
               RevCircleDropdownOptions.push({
                 label: circle['revenue-circle'],
                 value: circle.code,
-                type: district,
               });
             }
           );
         }
       }
-    } else {
-      geographiesData.data?.getDistrictRevCircle?.forEach(
-        (geography: { district: string; code: string }) => {
-          DistrictDropDownOption.push({
-            label: geography.district,
-            value: geography.code ? geography.code : 'NA',
-          });
-        }
-      );
+      return RevCircleDropdownOptions;
     }
-  }
+    geographiesData.data?.getDistrictRevCircle?.forEach(
+      (geography: { district: string; code: string }) => {
+        DistrictDropDownOption.push({
+          label: geography.district,
+          value: geography.code ? geography.code : 'NA',
+        });
+      }
+    );
+    return DistrictDropDownOption;
+  }, [geographiesData.data?.getDistrictRevCircle, boundarySelected]);
+
+  React.useEffect(() => {
+    if (geographiesData.data) {
+      const regionOptions = getRegionOptions();
+      setRegionOptions(regionOptions);
+    }
+  }, [geographiesData.data, getRegionOptions, boundarySelected]);
+
+  const FilterOptions = [
+    {
+      title: 'Boundary',
+      value: 'boundary',
+      options: [
+        { label: 'District', value: 'district' },
+        { label: 'Revenue Circle', value: 'revenue-circle' },
+      ],
+      type: 'radio-button',
+    },
+    {
+      title: 'Region',
+      value: 'region',
+      options: regionOptions,
+      type: 'radio-button',
+    },
+    {
+      title: 'Month',
+      value: 'month',
+      type: 'month-picker',
+    },
+  ];
 
   const toggleDrawer = () => {
     setIsDrawerOpen(!isDrawerOpen);
   };
 
-  function boundarySelection() {
-    return (
-      <>
-        <RadioGroup
-          onChange={(e) => {
-            setBoundary(e, { shallow: false });
-            setRegion([]);
-            setSelectedGroup([]);
-          }}
-          name="District"
-          defaultValue="district"
-        >
-          <RadioItem value="district">District</RadioItem>
-          <RadioItem value="revenue-circle">Revenue Circle</RadioItem>
-        </RadioGroup>
-      </>
-    );
-  }
-
-  function onRegionChange(selectedOptions: any) {
-    const val = selectedOptions.map((option: any) => option.value);
-
-    const group = selectedOptions.map((option: any) => option?.type ?? '');
-
-    setSelectedGroup(group);
-    setRegion(val);
-  }
-
-  const getDistrictOptions = () => {
-    const updatedDistrictDropDownOption = DistrictDropDownOption.map(
-      (item: any) => {
-        if (region?.length === 4 && boundary === 'district') {
-          return { ...item, disabled: true };
-        }
-
-        return item;
-      }
-    );
-    return updatedDistrictDropDownOption;
+  const handleSearchChange = (value: string) => {
+    if (value) {
+      const filtered = regionOptions.filter((item) =>
+        item?.label?.toLowerCase().includes(value?.toLowerCase())
+      );
+      setRegionOptions(filtered);
+    } else {
+      const regionOptions = getRegionOptions();
+      setRegionOptions(regionOptions);
+    }
   };
 
-  function RegionSelection() {
-    const options =
-      boundary === 'revenue-circle'
-        ? RevCircleDropdownOptions
-        : getDistrictOptions();
-
-    console.log('OPTION^^^^^^', options);
-    return (
-      <text>HII</text>
-      // <>
-      //   <RadioGroup
-      //     onChange={(selectedOption) => {
-      //       onRegionChange(selectedOption);
-      //     }}
-      //     name="Region"
-      //     defaultValue=""
-      //     className="overflow overflow-y-scroll"
-      //   >
-      //     {options.map((option, index) => (
-      //       <RadioItem key={index} value={option}>
-      //         {option.label}
-      //       </RadioItem>
-      //     ))}
-      //   </RadioGroup>
-      // </>
-    );
-  }
-
-  type Opt = 'boundaries' | 'region' | 'month';
-  const [selectedOption, setSelectedOption] = useState<Opt | null>(null);
-
-  const renderSelection = () => {
-    switch (selectedOption) {
-      case 'boundaries':
-        return boundarySelection();
-      case 'region':
-        //render search also: CORRECTION
-        return RegionSelection();
-      // case 'month':
-      // return <MonthSelection />; /
-      default:
-        return null;
-    }
+  const handleApplyFilters = () => {
+    setRegion(regionSelected, { shallow: false });
+    setBoundary(boundarySelected, { shallow: false });
+    setTimePeriod(timePeriodSelected, { shallow: false });
   };
 
   return (
@@ -239,45 +193,49 @@ export function FilterComp({
       <Drawer open={isDrawerOpen}>
         <DrawerContent>
           <DrawerHeader className=" h-[56px] border-b-1 border-solid border-[#C9CCCF]">
-            <DrawerTitle className="flex flex-row justify-between ">
+            <DrawerTitle className="flex justify-between ">
               <Text variant="headingMd">Options</Text>
               <IconButton
                 icon={Icons.cross}
                 onClick={toggleDrawer}
                 color="default"
               >
-                Menu
+                Close
               </IconButton>
             </DrawerTitle>
           </DrawerHeader>
-          <DrawerDescription className="flex flex-row">
-            <div className="g-8 flex w-[108px] flex-col items-start self-stretch border-r-1 border-solid border-[#BDBDBD] px-2 py-4 ">
-              <Button
-                className=" bg-[#ffffff]  text-textDefault"
-                size="slim"
-                // onClick={() => boundarySelection()}
-                onClick={() => setSelectedOption('boundaries')}
-              >
-                Boundary
-              </Button>
-              <Button
-                className=" bg-[#ffffff]  text-textDefault"
-                size="slim"
-                onClick={() => setSelectedOption('region')}
-              >
-                Region
-              </Button>
-              <Button
-                className=" bg-[#ffffff]  text-textDefault"
-                size="slim"
-                // onClick={() => monthSelection()}
-                onClick={() => setSelectedOption('month')}
-              >
-                Month
-              </Button>
+          <DrawerDescription className="flex h-[276px]">
+            <div className="flex flex-col gap-3 border-x-1 border-solid border-borderSubdued p-4">
+              {FilterOptions.map((item, index) => (
+                <div key={`${item.value}-${index}`}>
+                  <Button
+                    className={cn('min-w-[40px] text-textDefault')}
+                    size="slim"
+                    fullWidth
+                    kind="tertiary"
+                    variant="interactive"
+                    // onClick={() => boundarySelection()}
+                    onClick={() => setSelectedOption(item?.value)}
+                  >
+                    <Text>{item.title}</Text>
+                  </Button>
+                </div>
+              ))}
             </div>
-            <div className="flex w-[108px] flex-row gap-1 overflow-y-scroll py-4 pl-3">
-              {renderSelection()}
+            <div className="w-full overflow-x-auto p-4">
+              <RenderOptions
+                filterOptions={FilterOptions}
+                selectedOption={selectedOption}
+                boundary={boundary}
+                timePeriod={timePeriod}
+                timePeriodData={timePeriods}
+                setBoundarySelected={setBoundarySelected}
+                setRegionSelected={setRegionSelected}
+                setTimePeriodSelected={setTimePeriodSelected}
+                handleInputChangeCallback={(value: string) =>
+                  handleSearchChange(value)
+                }
+              />
             </div>
           </DrawerDescription>
           <DrawerFooter className="flex flex-row justify-between border-t-1 border-solid border-[#BDBDBD]">
@@ -289,11 +247,12 @@ export function FilterComp({
                 Clear All
               </Text>
             </Button>
-            <DrawerClose
-              onClick={toggleDrawer}
-              className="border-[#71E57D] bg-[#71E57D]"
-            >
-              <Button className=" bg-[#71E57D]" size="large">
+            <DrawerClose onClick={toggleDrawer} asChild>
+              <Button
+                onClick={handleApplyFilters}
+                className=" bg-[#71E57D]"
+                size="large"
+              >
                 <Text variant="bodyLg" fontWeight="bold" color="default">
                   Apply
                 </Text>
@@ -305,3 +264,98 @@ export function FilterComp({
     </>
   );
 }
+
+export const RenderOptions = ({
+  filterOptions,
+  selectedOption,
+  boundary,
+  timePeriod,
+  timePeriodData,
+  handleInputChangeCallback,
+  setBoundarySelected,
+  setRegionSelected,
+  setTimePeriodSelected,
+}: any) => {
+  const [searchQuery, setSearchQuery] = useState('');
+
+  const findSelectedValue = filterOptions.filter(
+    (opt: { value: string }) => opt.value === selectedOption
+  );
+  const type = findSelectedValue[0]['type'];
+  const value = findSelectedValue[0]['value'];
+  const options = findSelectedValue[0]['options'];
+
+  const onRadioButtonChange = (selectedValue: string, value: string) => {
+    if (value === 'boundary') {
+      setBoundarySelected(selectedValue);
+      setRegionSelected('');
+    } else {
+      setRegionSelected(selectedValue);
+    }
+  };
+
+  let minDate: string, maxDate: string;
+
+  const datesArray = timePeriodData?.data?.getDataTimePeriods.map(
+    (date: any) => {
+      const [year, month] = date.value.split('_');
+      return new Date(parseInt(year), parseInt(month));
+    }
+  );
+  const timestamps = datesArray.map((date: any) => date.getTime());
+  // Find the minimum and maximum timestamps
+  const minTimestamp = Math.min(...timestamps);
+  const maxTimestamp = Math.max(...timestamps);
+
+  // Convert the timestamps back to dates
+  minDate = formatDate(minTimestamp, true);
+  maxDate = formatDate(maxTimestamp, true);
+
+  switch (type) {
+    case 'radio-button':
+      return (
+        <React.Fragment>
+          {value === 'region' && (
+            <TextField
+              label="Search"
+              name="name"
+              value={searchQuery}
+              onChange={(e) => {
+                setSearchQuery(e), handleInputChangeCallback(e);
+              }}
+            />
+          )}
+          <RadioGroup
+            onChange={(e) => {
+              onRadioButtonChange(e, value);
+            }}
+            name={value}
+            defaultValue={boundary}
+          >
+            {options?.map(
+              (item: { value: string; label: string }, index: number) => (
+                <RadioItem key={`${item.value}-${index}`} value={item.value}>
+                  {item.label}
+                </RadioItem>
+              )
+            )}
+          </RadioGroup>
+        </React.Fragment>
+      );
+    case 'month-picker':
+      return (
+        <MonthPicker
+          name="time-period-select"
+          defaultValue={parseDate('2023-08-01')}
+          label="Select Month"
+          minValue={parseDate(minDate || '2023-01-04')}
+          maxValue={parseDate(maxDate || '2023-01-04')}
+          onChange={(date) => {
+            setTimePeriodSelected(
+              `${date.year}_${date.month < 10 ? `0${date.month}` : `${date.month}`}`
+            );
+          }}
+        />
+      );
+  }
+};
