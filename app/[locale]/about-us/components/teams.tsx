111feaddf15e4cedddaacb34928f2165ccb1a070
sanjaypinna <sanjaypinna265@gmail.com>
2024-09-19 17:05:02 +0530

    fix margin

--- app/[locale]/about-us/components/teams.tsx
+++ app/[locale]/about-us/components/teams.tsx
@@ -80,7 +80,7 @@ export function TheTeam() {
         <Text variant="heading2xl" fontWeight="bold" color="default">
           The Team
         </Text>
-        <div className="mt-2 grid grid-cols-1 gap-10 md:grid-cols-2 lg:grid-cols-3 ">
+        <div className="mt-6 grid grid-cols-1 gap-10 md:grid-cols-2 lg:grid-cols-3 ">
           {teamMembers.map((member, index) => (
             <div key={index} className={styles.card}>
               <Image
