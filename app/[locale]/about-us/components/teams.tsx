c72e1260343ceb78eac0e435c333fe297fee57bf
sanjaypinna <sanjaypinna265@gmail.com>
2024-09-19 17:04:23 +0530

    update about us page

--- app/[locale]/about-us/components/teams.tsx
+++ app/[locale]/about-us/components/teams.tsx
@@ -13,45 +13,33 @@ interface TeamMember {
 
 const teamMembers: TeamMember[] = [
   {
-    name: 'Abhinandita',
-    role: 'Senior Product Designer',
-    imageUrl: '/teams/abhinandita.jpg',
+    name: 'Gaurav',
+    role: 'Founder',
+    imageUrl: '/teams/gaurav.jpg',
   },
-  { name: 'Aashim', role: 'Frontend Engineer', imageUrl: '/teams/aashim.jpg' },
-  {
-    name: 'Aparna',
-    role: 'Associate Product Designer',
-    imageUrl: '/teams/aparna.jpg',
-  },
-  { name: 'Archit', role: 'Backend Engineer', imageUrl: '/teams/archit1.jpg' },
-  { name: 'Deepthi Chand', role: 'Founder', imageUrl: '/teams/dc.jpg' },
-  { name: 'Jeeno', role: 'Senior Researcher', imageUrl: '/teams/jeeno.jpg' },
   { name: 'Kabeer', role: 'Initiative Lead', imageUrl: '/teams/kabeer.jpg' },
-  {
-    name: 'Kakoli',
-    role: 'Partnership & Outreach Manager',
-    imageUrl: '/teams/kakoli.jpg',
-  },
   { name: 'Nupura', role: 'Design Lead', imageUrl: '/teams/nupura.jpeg' },
+
   {
     name: 'Rakhi',
     role: 'Associate Initiative Lead',
     imageUrl: '/teams/rakhi.jpg',
   },
+  { name: 'Deepthi Chand', role: 'Founder', imageUrl: '/teams/dc.jpg' },
   {
     name: 'Ruthvik',
     role: 'Associate Lead Engineer',
     imageUrl: '/teams/ruthvik.jpg',
   },
   {
-    name: 'Sai Krishna',
-    role: 'Senior Data Engineer',
-    imageUrl: '/teams/sai.png',
+    name: 'Aparna',
+    role: 'Associate Product Designer',
+    imageUrl: '/teams/aparna.jpg',
   },
   {
-    name: 'Saqib',
-    role: 'Quality Assurance Engineer',
-    imageUrl: '/teams/saqib.jpg',
+    name: 'Sanjay',
+    role: 'Frontend Engineer',
+    imageUrl: '/teams/sanjay.jpg',
   },
   {
     name: 'Sumit',
@@ -59,89 +47,61 @@ const teamMembers: TeamMember[] = [
     imageUrl: '/teams/sumit.jpg',
   },
   { name: 'Swati', role: 'Frontend Engineer', imageUrl: '/teams/swati1.jpg' },
+  {
+    name: 'Saqib',
+    role: 'Quality Assurance Engineer',
+    imageUrl: '/teams/saqib.jpg',
+  },
+  {
+    name: 'Abhinandita',
+    role: 'Senior Product Designer',
+    imageUrl: '/teams/abhinandita.jpg',
+  },
+  {
+    name: 'Kakoli',
+    role: 'Partnership & Outreach Manager',
+    imageUrl: '/teams/kakoli.jpg',
+  },
+  { name: 'Archit', role: 'Backend Engineer', imageUrl: '/teams/archit1.jpg' },
+  {
+    name: 'Sai Krishna',
+    role: 'Senior Data Engineer',
+    imageUrl: '/teams/sai.png',
+  },
+  { name: 'Jeeno', role: 'Senior Researcher', imageUrl: '/teams/jeeno.jpg' },
+  { name: 'Aashim', role: 'Frontend Engineer', imageUrl: '/teams/aashim.jpg' },
 ];
 
 export function TheTeam() {
   return (
-    <section className="flex flex-col flex-wrap py-14 ">
-      <MediaRendering minWidth="1024" maxWidth={null}>
-        {/* DESKTOP  */}
-        <div className="container mb-2 flex flex-col gap-8 ">
-          <Text variant="heading2xl" fontWeight="bold" color="default">
-            The Team
-          </Text>
-          <div className={styles.grid}>
-            {teamMembers
-              .sort((a: any, b: any) => a.name - b.name)
-              .map((member, index) => (
-                <div key={index} className={styles.card}>
-                  <Image
-                    src={member.imageUrl}
-                    alt={member.name}
-                    height={100}
-                    width={200}
-                    className="object-contain pt-4 "
-                  />
-                  <div className="flex w-full flex-col gap-2 pl-8 pt-4 ">
-                    <Text
-                      variant="headingLg"
-                      fontWeight="semibold"
-                      color="default"
-                    >
-                      {member.name}
-                    </Text>
-                    <Text
-                      variant="headingMd"
-                      fontWeight="regular"
-                      color="subdued"
-                    >
-                      {member.role}
-                    </Text>
-                  </div>
-                </div>
-              ))}
-          </div>
+    <section className=" py-14 ">
+      {/* DESKTOP  */}
+      <div className="container ">
+        <Text variant="heading2xl" fontWeight="bold" color="default">
+          The Team
+        </Text>
+        <div className="mt-2 grid grid-cols-1 gap-10 md:grid-cols-2 lg:grid-cols-3 ">
+          {teamMembers.map((member, index) => (
+            <div key={index} className={styles.card}>
+              <Image
+                src={member.imageUrl}
+                alt={member.name}
+                height={100}
+                width={200}
+                className="object-contain pt-4 "
+              />
+              <div className="flex w-full flex-col gap-2 pl-8 pt-4 ">
+                <Text variant="headingLg" fontWeight="semibold" color="default">
+                  {member.name}
+                </Text>
+                <Text variant="headingMd" fontWeight="regular" color="subdued">
+                  {member.role}
+                </Text>
+              </div>
+            </div>
+          ))}
         </div>
-      </MediaRendering>
-      <MediaRendering minWidth={null} maxWidth="1023">
-        {/* mobile */}
-        <div className="container mb-2 flex flex-col gap-8 ">
-          <Text variant="heading2xl" fontWeight="bold" color="default">
-            The Team
-          </Text>
-          <div className={styles.grid}>
-            {teamMembers
-              .sort((a, b) => a.name.localeCompare(b.name))
-              .map((member, index) => (
-                <div key={index} className={styles.card}>
-                  <Image
-                    src={member.imageUrl}
-                    alt={member.name}
-                    height={100}
-                    width={200}
-                    className="object-contain pt-4 "
-                  />
-                  <div className="flex w-full flex-col gap-2 pl-8 pt-4 ">
-                    <Text
-                      variant="headingLg"
-                      fontWeight="semibold"
-                      color="default"
-                    >
-                      {member.name}
-                    </Text>
-                    <Text
-                      variant="headingMd"
-                      fontWeight="regular"
-                      color="subdued"
-                    >
-                      {member.role}
-                    </Text>
-                  </div>
-                </div>
-              ))}
-          </div>
-        </div>
-      </MediaRendering>
+      </div>
     </section>
   );
 }
