ee02f258945d24382a54d55cd4b1155cd1a1a0c5
sanjaypinna <sanjaypinna265@gmail.com>
2024-09-19 14:25:10 +0530

    add partners section

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,11 +1,12 @@
 import { About, HeroSection, QuickLinks } from './components';
 import DataStories from './components/data-stories';
 import { DatasetCatalog } from './components/dataset-catalog';
+import Partners from './components/partners';
 import Resources from './components/resources';
 
 export default function Home() {
   return (
-    <main className="bg-baseGreenSolid5 pb-0.5">
+    <main className="bg-baseGreenSolid5 ">
       <div className="inline-flex w-full flex-col items-center ">
         <HeroSection />
         <QuickLinks />
@@ -13,6 +14,7 @@ export default function Home() {
         <Resources />
         <DataStories />
         <About />
+        <Partners />
       </div>
     </main>
   );
