dae707f586767bc5a00ba8876f59f33333f1b2b8
sanjaypinna <sanjaypinna265@gmail.com>
2024-09-27 16:52:04 +0530

    add texts

--- config/consts.ts
+++ config/consts.ts
@@ -84,7 +84,7 @@ const AboutTextContentTwo =
 const AboutTextContentThree = 'We have a 3-pronged approach: ';
 
 const HeroSectionText =
-  'Discover insights, assess risks and build disaster resilience for data-driven disaster risk reduction (DRR)';
+  'Discover insights, assess risks and build disaster resilience for data-driven disaster risk reduction';
 
 const AnalyticsQuickLinksText =
   'Gain insights from the IDS-DRR data model to understand the flood-risk profiles of districts and sub-district regions in various states across India.';
@@ -116,6 +116,9 @@ const CollaboratingPartnerTextOne =
 const CollaboratingPartnerTextTwo =
   'ASDMA has also notified the District Disaster Management Authority in all the 33 districts of Assam and placed officers for carrying out disaster management activities at the district.';
 
+const CollaboratingPartnerHPText =
+  'The HP SDMA has been constituted under the chairmanship of Chief  Minister of Himachal Pradesh. Revenue Minister has been nominated as  member. Chief Secretary is the Chief Executive Officer of the SDMA. ACS  cum FC (Revenue), Principal Secretary (Home), Principal Secretary (PWD/I  & PH), Principal Secretary (Health) and Director General of Police  have also been notified as member. Additional Chief Secretary (Revenue)  is the Member Secretary of the Authority.';
+
 const TheRockefellerFoundationTextOne =
   'The Rockefeller Foundation is a pioneering philanthropy built on collaborative partnerships at the frontiers of science, technology, and innovation that enable individuals, families, and communities to flourish. We make big bets to promote the well-being of humanity. Today, we are focused on advancing human opportunity and reversing the climate crisis by transforming systems in food, health, energy, and finance. ';
 const TheRockefellerFoundationTextTwo =
@@ -125,6 +128,16 @@ const OpenContractingPartnershipTextOne =
 
 const OpenContractingPartnershipTextTwo =
   'OCP is unique in bringing these three approaches together and at scale. OCP makes sure public money is spent openly, fairly and effectively on public contracts, delivering fundamentally better public spending outcomes that benefit people and protect the planet.';
+
+const PJMcPartnershipText =
+  'The Patrick J. McGovern Foundation (PJMF) is a philanthropic organization dedicated to advancing artificial intelligence and data science solutions to create a thriving, equitable, and sustainable future for all. PJMF works in partnership with public, private, and social institutions to drive progress on our most pressing challenges, including digital health, climate change, broad digital access, and data maturity in the social sector.';
+
+const CDLPartnershipTextOne =
+  'We are a research lab working at the  intersection of data, tech, design and social science to strengthen the  course of civic engagements in India.';
+const CDLPartnershipTextTwo =
+  'We work to harness the potential of open knowledge movements and better enable citizens to engage in matters of public reform.';
+const CDLPartnershipTextThree =
+  'We aim to grow data and tech literacy of governments, non-profits,  think-tanks, media houses, universities, and more to enable data-driven  decision making at scale.';
 
 export {
   platformName,
@@ -161,4 +174,9 @@ export {
   DatasetCatalogText,
   ResourcesSectionText,
   DataStoriesText,
+  CollaboratingPartnerHPText,
+  PJMcPartnershipText,
+  CDLPartnershipTextOne,
+  CDLPartnershipTextTwo,
+  CDLPartnershipTextThree,
 };
