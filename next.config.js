49af238d7b2eec1ff18a7775b52f723f0175174a
sanjaypinna <sanjaypinna265@gmail.com>
2024-09-30 14:55:59 +0530

    update data-stories

--- next.config.js
+++ next.config.js
@@ -3,6 +3,15 @@ const withNextIntl = require('next-intl/plugin')();
 
 const nextConfig = {
   transpilePackages: ['opub-ui'],
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'miro.medium.com',
+        port: '',
+      },
+    ],
+  },
 };
 
 module.exports = withNextIntl(nextConfig);
