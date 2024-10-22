71aae4c5361bbc79465152c718bc3c8a3b2b9100
Swati S <s.swati.5293@gmail.com>
2024-10-22 14:54:11 +0530

    mobile analytics layout rebasing

--- app/[locale]/analytics/components/map-component.tsx
+++ app/[locale]/analytics/components/map-component.tsx
@@ -218,6 +218,14 @@ export const MapComponent = ({
     }
   }, [map, districtCode]);
 
+  if (mapDataloading || revenueMapDataLoading)
+    return (
+      <div className="flex h-full flex-col place-content-center items-center">
+        <Spinner color="highlight" />
+        <Text>Loading...</Text>
+      </div>
+    );
+
   return (
     <>
       {/* Mobile View */}
