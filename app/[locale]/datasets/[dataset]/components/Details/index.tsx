3083e241938b84cd930228c34abdb6544f0275f3
sanjaypinna <sanjaypinna265@gmail.com>
2024-10-24 09:04:44 +0530

    add renderGeoJSON fxn to show charts and disable share and download according to chart type

--- app/[locale]/datasets/[dataset]/components/Details/index.tsx
+++ app/[locale]/datasets/[dataset]/components/Details/index.tsx
@@ -1,6 +1,10 @@
+import { useRef } from 'react';
 import Link from 'next/link';
 import { useParams } from 'next/navigation';
+import { renderGeoJSON } from '@/geo_json/render_geojson';
 import { useQuery } from '@tanstack/react-query';
+import ReactECharts from 'echarts-for-react';
+import * as echarts from 'echarts/core';
 import {
   Button,
   Carousel,
@@ -13,7 +17,6 @@ import {
   Spinner,
   Text,
 } from 'opub-ui';
-import { BarChart } from 'opub-ui/viz';
 
 import { CHARTS_QUERY } from '@/config/graphql/dataset-queries';
 import { GraphQL } from '@/lib/api';
@@ -39,6 +42,29 @@ const Details = () => {
       )
   );
 
+  const chartRef = useRef<ReactECharts>(null);
+
+  const renderChart = (item: any) => {
+    if (
+      item?.chartType === 'ASSAM_DISTRICT' ||
+      item?.chartType === 'ASSAM_RC'
+    ) {
+      // Register the map
+      echarts.registerMap(
+        item?.chartType.toLowerCase(),
+        renderGeoJSON(item.chartType.toLowerCase())
+      );
+    }
+
+    return (
+      <ReactECharts
+        option={item.chart}
+        ref={chartRef}
+        style={{ height: '450px' }}
+      />
+    );
+  };
+
   return (
     <div className="mb-8 flex w-full flex-col gap-4 p-2">
       {isLoading ? (
@@ -57,72 +83,32 @@ const Details = () => {
                   {data?.chartsDetails.map((item: any, index: any) => (
                     <CarouselItem key={index} className="m-auto">
                       <div className="w-full border-2 border-solid border-baseGraySlateSolid4 bg-surfaceDefault p-6 text-center shadow-basicLg max-sm:p-2">
-                        <div className="lg:p-10">
-                          <BarChart options={item.chart} height={'450px'} />
-                        </div>
+                        <div className=" lg:p-10">{renderChart(item)} </div>
                         <div className="flex items-center justify-between gap-2 max-sm:flex-wrap">
                           <div className="flex flex-col gap-1 py-2 text-start">
                             <Text className="font-semi-bold">{item.name}</Text>
                             <Text>{item.description}</Text>
                           </div>
-                          <div className="flex gap-2">
-                            <Menu
-                              trigger={
-                                <Button
-                                  monochrome={true}
-                                  kind="secondary"
-                                  className="bg-[#E0F8E2] p-2"
-                                >
-                                  <Icon
-                                    source={Icons.share}
-                                    size={20}
-                                    color="default"
-                                  />
-                                </Button>
-                              }
-                              items={[
-                                {
-                                  content: 'Facebook',
-                                  icon: Icons.IconBrandFacebook,
-                                  onAction: () =>
-                                    window.open(
-                                      `https://www.facebook.com/sharer.php?u=${process.env.NEXT_PUBLIC_BACKEND_URL}/api/download/chart/${item.id}/`
-                                    ),
-                                },
-                                {
-                                  content: 'LinkedIn',
-                                  icon: Icons.IconBrandLinkedin,
-                                  onAction: () =>
-                                    window.open(
-                                      `https://www.linkedin.com/shareArticle?url=${process.env.NEXT_PUBLIC_BACKEND_URL}/api/download/chart/${item.id}/`
-                                    ),
-                                },
-                                {
-                                  content: 'Twitter',
-                                  icon: Icons.IconBrandX,
-                                  onAction: () =>
-                                    window.open(
-                                      `https://twitter.com/intent/tweet?url=${process.env.NEXT_PUBLIC_BACKEND_URL}/api/download/chart/${item.id}/`
-                                    ),
-                                },
-                                {
-                                  content: 'Copy Link',
-                                  icon: Icons.link,
-                                  onAction: () =>
-                                    copyDefinedURL(
-                                      `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/download/chart/${item.id}`
-                                    ),
-                                },
-                              ]}
-                            />
-                            <Link
-                              href={`${process.env.NEXT_PUBLIC_BACKEND_URL}/api/download/chart/${item.id}`}
-                              target="_blank"
-                              className="flex justify-center"
-                            >
+                          {item.chartType === 'ASSAM_DISTRICT' ||
+                          item.chartType === 'ASSAM_RC' ? (
+                            <div className="flex gap-2">
+                              {' '}
+                              <Button
+                                monochrome={true}
+                                kind="secondary"
+                                className="bg-[#E0F8E2] p-2"
+                                disabled={true}
+                              >
+                                <Icon
+                                  source={Icons.share}
+                                  size={20}
+                                  color="default"
+                                />
+                              </Button>{' '}
                               <Button
                                 kind="secondary"
                                 className="bg-[#E0F8E2] p-2"
+                                disabled={true}
                               >
                                 <Icon
                                   source={Icons.download}
@@ -130,8 +116,76 @@ const Details = () => {
                                   color="default"
                                 />
                               </Button>
-                            </Link>
-                          </div>
+                            </div>
+                          ) : (
+                            <div className="flex gap-2">
+                              <Menu
+                                trigger={
+                                  <Button
+                                    monochrome={true}
+                                    kind="secondary"
+                                    className="bg-[#E0F8E2] p-2"
+                                  >
+                                    <Icon
+                                      source={Icons.share}
+                                      size={20}
+                                      color="default"
+                                    />
+                                  </Button>
+                                }
+                                items={[
+                                  {
+                                    content: 'Facebook',
+                                    icon: Icons.IconBrandFacebook,
+                                    onAction: () =>
+                                      window.open(
+                                        `https://www.facebook.com/sharer.php?u=${process.env.NEXT_PUBLIC_BACKEND_URL}/api/download/chart/${item.id}/`
+                                      ),
+                                  },
+                                  {
+                                    content: 'LinkedIn',
+                                    icon: Icons.IconBrandLinkedin,
+                                    onAction: () =>
+                                      window.open(
+                                        `https://www.linkedin.com/shareArticle?url=${process.env.NEXT_PUBLIC_BACKEND_URL}/api/download/chart/${item.id}/`
+                                      ),
+                                  },
+                                  {
+                                    content: 'Twitter',
+                                    icon: Icons.IconBrandX,
+                                    onAction: () =>
+                                      window.open(
+                                        `https://twitter.com/intent/tweet?url=${process.env.NEXT_PUBLIC_BACKEND_URL}/api/download/chart/${item.id}/`
+                                      ),
+                                  },
+                                  {
+                                    content: 'Copy Link',
+                                    icon: Icons.link,
+                                    onAction: () =>
+                                      copyDefinedURL(
+                                        `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/download/chart/${item.id}`
+                                      ),
+                                  },
+                                ]}
+                              />
+                              <Link
+                                href={`${process.env.NEXT_PUBLIC_BACKEND_URL}/api/download/chart/${item.id}`}
+                                target="_blank"
+                                className="flex justify-center"
+                              >
+                                <Button
+                                  kind="secondary"
+                                  className="bg-[#E0F8E2] p-2"
+                                >
+                                  <Icon
+                                    source={Icons.download}
+                                    size={20}
+                                    color="default"
+                                  />
+                                </Button>
+                              </Link>
+                            </div>
+                          )}
                         </div>
                       </div>
                     </CarouselItem>
