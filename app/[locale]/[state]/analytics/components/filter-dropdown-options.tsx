b02f1b67f567b9ed48570182f123f77322f4c4fa
bhavabhuthi <bhavabhuthi@riseup.net>
2025-02-05 14:03:58 +0530

    Move the district and revenue circle dropdown to a new component for reuse in chart view

---
+++ app/[locale]/[state]/analytics/components/filter-dropdown-options.tsx
@@ -0,0 +1,68 @@
+import { parseAsString, useQueryState } from 'next-usequerystate';
+import { Select } from 'opub-ui';
+
+import { toTitleCase } from '@/lib/utils';
+
+export interface Option {
+  disabled?: boolean;
+  value: string;
+  label: string;
+  districtCode?: string;
+}
+
+export default function FilterDropdownOptions({
+  currentSelectedState,
+  RevCircleDropdownOptions,
+  DistrictDropDownOption,
+}: {
+  currentSelectedState: any;
+  RevCircleDropdownOptions: Option[];
+  DistrictDropDownOption: Option[];
+}) {
+  const [districtCode, setDistrictCode] = useQueryState(
+    'district-code',
+    parseAsString.withDefault('')
+  );
+  const [revenueCode, setRevenueCode] = useQueryState('revenue-code');
+
+  const getRevenueCircleOptionsForDistrict = (districtCode: string) => {
+    const filterRevenueCircles = RevCircleDropdownOptions.filter(
+      (option: Option) => option.districtCode === districtCode
+    );
+
+    filterRevenueCircles.unshift({ label: '', value: '' });
+
+    return filterRevenueCircles;
+  };
+
+  return (
+    <>
+      <Select
+        label="Select District"
+        value={districtCode || ''}
+        name="district-select"
+        className=" flex-grow"
+        onChange={(e) => {
+          setDistrictCode(e, { shallow: false });
+        }}
+        options={DistrictDropDownOption}
+      />
+      <Select
+        label={`Select ${toTitleCase(currentSelectedState.child_type)}`}
+        value={revenueCode || ''}
+        placeholder={
+          !districtCode
+            ? 'Select a district to enable'
+            : `Select a ${toTitleCase(currentSelectedState.child_type)}`
+        }
+        name="revenue-circle-select"
+        className=" flex-grow"
+        disabled={!districtCode}
+        onChange={(e) => {
+          setRevenueCode(e, { shallow: false });
+        }}
+        options={getRevenueCircleOptionsForDistrict(districtCode) || []}
+      />
+    </>
+  );
+}
