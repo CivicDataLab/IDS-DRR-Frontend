0caca7834776dade1093b6050f9f17952a503d75
bhavabhuthi <bhavabhuthi@riseup.net>
2025-02-10 20:52:41 +0530

    add getLatestDate method to utils

--- app/[locale]/[state]/analytics/utils/utils.ts
+++ app/[locale]/[state]/analytics/utils/utils.ts
@@ -97,3 +97,26 @@ export function formatNumberToIndianSystem(input: number): string | number {
   const unit = input.toString().replace(match[0], '').trim();
   return unit ? `${formatted} ${unit}` : formatted;
 }
+
+
+export const getLatestDate = (dateStrings: string[]) => {
+  if (!dateStrings || dateStrings.length === 0) {
+    return process.env.NEXT_PUBLIC_TIME_PERIOD; // Handle empty array case
+  }
+
+  // Convert each 'yyyy_mm' string to a Date object
+  const dates = dateStrings.map((dateStr) => {
+    const [year, month] = dateStr.split('_'); // Split into year and month
+    return new Date(parseInt(year), parseInt(month) - 1); // Create a Date object (month is 0-indexed in JavaScript)
+  });
+
+  // Find the latest date
+  const latestDate = new Date(
+    Math.max(...dates.map((date) => date.getTime()))
+  );
+
+  // Convert the latest Date back to 'yyyy_mm' format
+  const year = latestDate.getFullYear();
+  const month = String(latestDate.getMonth() + 1).padStart(2, '0'); // Add leading zero for single-digit months
+  return `${year}-${month}-01`;
+};
