00ac42b9996a104949d3e6a37b29af70c5d0277d
bhavabhuthi <bhavabhuthi@riseup.net>
2025-02-10 20:55:30 +0530

    add flags for multi select of date and limits of calendar
    Also set calendars default values and set the search param based on the selection of date from single and multi date select

--- app/[locale]/[state]/analytics/components/filter-dropdown-options.tsx
+++ app/[locale]/[state]/analytics/components/filter-dropdown-options.tsx
@@ -1,7 +1,9 @@
+import { parseDate } from '@internationalized/date';
 import { parseAsString, useQueryState } from 'next-usequerystate';
-import { Select } from 'opub-ui';
+import { MonthPicker, MultiMonthPicker, Select } from 'opub-ui';
 
-import { toTitleCase } from '@/lib/utils';
+import { formatDate, toTitleCase } from '@/lib/utils';
+import { getLatestDate } from '../utils/utils';
 
 export interface Option {
   disabled?: boolean;
@@ -14,10 +16,14 @@ export default function FilterDropdownOptions({
   currentSelectedState,
   RevCircleDropdownOptions,
   DistrictDropDownOption,
+  monthMulti = false,
+  timeLimits,
 }: {
   currentSelectedState: any;
   RevCircleDropdownOptions: Option[];
   DistrictDropDownOption: Option[];
+  monthMulti?: boolean;
+  timeLimits: any;
 }) {
   const [districtCode, setDistrictCode] = useQueryState(
     'district-code',
@@ -35,35 +41,108 @@ export default function FilterDropdownOptions({
     return filterRevenueCircles;
   };
 
+  let minDate, maxDate;
+  // Below is code to set limits to the calendar
+  if (timeLimits.data) {
+    const datesArray = timeLimits?.data?.getDataTimePeriods.map((date: any) => {
+      const [year, month] = date.value.split('_');
+      return new Date(parseInt(year), parseInt(month));
+    });
+    const timestamps = datesArray.map((date: any) => date.getTime());
+    // Find the minimum and maximum timestamps
+    const minTimestamp = Math.min(...timestamps);
+    const maxTimestamp = Math.max(...timestamps);
+
+    // Convert the timestamps back to dates
+    minDate = formatDate(minTimestamp, true);
+    maxDate = formatDate(maxTimestamp, true);
+  }
+
+  const [selectedTimePeriod, setSelectedTimePeriod] = useQueryState<string[]>(
+    'time-period',
+    {
+      parse: (value) => {
+        return value.split(',');
+      },
+    }
+  );
+
   return (
-    <>
-      <Select
-        label="Select District"
-        value={districtCode || ''}
-        name="district-select"
-        className=" flex-grow"
-        onChange={(e) => {
-          setDistrictCode(e, { shallow: false });
-          setRevenueCode('');
-        }}
-        options={DistrictDropDownOption}
-      />
-      <Select
-        label={`Select ${toTitleCase(currentSelectedState.child_type)}`}
-        value={revenueCode || ''}
-        placeholder={
-          !districtCode
-            ? 'Select a district to enable'
-            : `Select a ${toTitleCase(currentSelectedState.child_type)}`
-        }
-        name="revenue-circle-select"
-        className=" flex-grow"
-        disabled={!districtCode}
-        onChange={(e) => {
-          setRevenueCode(e, { shallow: false });
-        }}
-        options={getRevenueCircleOptionsForDistrict(districtCode) || []}
-      />
-    </>
+    <div>
+      <div className="mb-2 flex items-start justify-evenly gap-3 p-4 pb-0 pt-0">
+        <Select
+          label="Select District"
+          value={districtCode || ''}
+          name="district-select"
+          className="flex-1"
+          onChange={(e) => {
+            setDistrictCode(e, { shallow: false });
+            setRevenueCode('');
+          }}
+          options={DistrictDropDownOption}
+        />
+        <Select
+          label={`Select ${toTitleCase(currentSelectedState.child_type)}`}
+          value={revenueCode || ''}
+          placeholder={
+            !districtCode
+              ? 'Select a district to enable'
+              : `Select a ${toTitleCase(currentSelectedState.child_type)}`
+          }
+          name="revenue-circle-select"
+          className="flex-1"
+          disabled={!districtCode}
+          onChange={(e) => {
+            setRevenueCode(e, { shallow: false });
+          }}
+          options={getRevenueCircleOptionsForDistrict(districtCode) || []}
+        />
+
+        <div className="flex-1">
+          {monthMulti ? (
+            <MultiMonthPicker
+              // TODO: add support for name, className, minValue and maxValue in opub-ui
+              // name="time-period-select"
+              // className="flex-1"
+              selectedValues={
+                selectedTimePeriod?.map((timePeriod: string) => {
+                  const [year, month] = timePeriod.split('_');
+                  return parseDate(`${year}-${month.padStart(2, '0')}-01`);
+                }) || []
+              }
+              label="Select Months"
+              // minValue={parseDate(minDate || '2023-01-04')}
+              // maxValue={parseDate(maxDate || '2023-01-04')}
+              onChange={(dates: any) => {
+                setSelectedTimePeriod(
+                  dates.map(
+                    (date: any) =>
+                      `${date.year}_${date.month < 10 ? `0${date.month}` : `${date.month}`}`
+                  )
+                );
+              }}
+            />
+          ) : (
+            <MonthPicker
+              name="time-period-select"
+              defaultValue={parseDate(
+                getLatestDate(selectedTimePeriod || []) || '2023-08-01'
+              )}
+              label="Select Month"
+              minValue={parseDate(minDate || '2023-01-04')}
+              maxValue={parseDate(maxDate || '2023-01-04')}
+              onChange={(date) => {
+                setSelectedTimePeriod(
+                  [
+                    `${date.year}_${date.month < 10 ? `0${date.month}` : `${date.month}`}`,
+                  ],
+                  { shallow: false }
+                );
+              }}
+            />
+          )}
+        </div>
+      </div>
+    </div>
   );
 }
