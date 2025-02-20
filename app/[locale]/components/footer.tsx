20d40ea7b1711ed1d37521dc82889429593391f7
sswadi <84518473+sswadi@users.noreply.github.com>
2025-02-20 17:28:44 +0530

    Update IDS-DRR logo on Header and Footer for both Desktop and Mobile

--- app/[locale]/components/footer.tsx
+++ app/[locale]/components/footer.tsx
@@ -11,7 +11,7 @@ export const Footer = () => {
     <footer className="flex flex-col flex-wrap gap-4 bg-backgroundSolidDark px-5 py-4 md:flex-row md:justify-between md:px-10 md:py-8">
       <div className="flex flex-row items-center gap-3">
         <Image
-          src="/logo/IDSLogo.png"
+          src="/logo/IDS-Platform-Logo.png"
           width={245}
           height={24}
           alt="IDS-DRR Logo"
--- components/main-nav.tsx
+++ components/main-nav.tsx
@@ -28,7 +28,7 @@ export function MainNav({ data }: { data: MainConfig }) {
           <NavLink href={data.homeUrl}>
             <div className="flex items-center gap-2">
               <Image
-                src="/logo/IDSLogo.png"
+                src="/logo/IDS-Platform-Logo.png"
                 width={245}
                 height={24}
                 alt="IDS-DRR Logo"
