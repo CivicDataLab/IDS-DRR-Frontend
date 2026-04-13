ce705f45c1abe71e402e4c1b708e4d2157cb9c71
James McKinney <26463+jpmckinney@users.noreply.github.com>
2026-04-13 19:37:29 -0400

    feat: Make the DataSpace BACKEND_URL optional. Remove the unused import of the file generated from the BACKEND_URL GraphQL API.

--- config/codegen.ts
+++ config/codegen.ts
@@ -1,21 +1,26 @@
 import type { CodegenConfig } from '@graphql-codegen/cli';
 
+const generates: CodegenConfig['generates'] = {
+  './gql/generated/analytics/': {
+    documents: 'config/**/analaytics-queries.ts',
+    schema: `${process.env.DATA_MANAGEMENT_LAYER_URL}/graphql`,
+    preset: 'client',
+    plugins: [],
+  },
+};
+
+if (process.env.BACKEND_URL) {
+  generates['./gql/generated/datasets/'] = {
+    documents: 'config/**/dataset-queries.ts',
+    schema: `${process.env.BACKEND_URL}/api/graphql`,
+    preset: 'client',
+    plugins: [],
+  };
+}
+
 const config: CodegenConfig = {
   overwrite: true,
-  generates: {
-    './gql/generated/datasets/': {
-      documents: 'config/**/dataset-queries.ts',
-      schema: `${process.env.BACKEND_URL}/api/graphql`,
-      preset: 'client',
-      plugins: [],
-    },
-    './gql/generated/analytics/': {
-      documents: 'config/**/analaytics-queries.ts',
-      schema: `${process.env.DATA_MANAGEMENT_LAYER_URL}/graphql`,
-      preset: 'client',
-      plugins: [],
-    },
-  },
+  generates,
 };
 
 export default config;
