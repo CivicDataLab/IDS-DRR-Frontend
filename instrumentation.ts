fda7c15889263b0bf517b2b172bbff7849229365
James McKinney <26463+jpmckinney@users.noreply.github.com>
2026-04-22 14:39:06 -0400

    chore: Reduce noise at project root
    - Upgrade Sentry from 8 to 10:
    
      - sourcemaps.deleteSourcemapsAfterUpload: true is removed as it is default
      - disableLogger and automaticVercelMonitors are moved as they are deprecated
      - autoSessionTracking is removed as it is obsolete
      - add onRouterTransitionStart as it is required
      - sentry.server.config.ts and sentry.edge.config.ts become instrumentation.ts
      - sentry.client.config.ts becomes instrumentation-client.ts
    
    - Move .eslintrc.json, .prettierrc and postcss.config.js into package.json
    - Move global.d.ts to types/global.d.ts, as tsconfig already scans types/
    - Remove obsolete prettier configuration:
    
      - importOrderBuiltinModulesToTop: true is retained via "<BUILTIN_MODULES>"
      - importOrderSeparation: false is retained via ""
      - importOrderSortSpecifiers: true is default in V4
      - importOrderMergeDuplicateImports: true is default in V4
      - importOrderCombineTypeAndValueImports: true is default in V4
    
    - .prettierignore is not needed: https://prettier.io/docs/ignore/
    
      > Prettier will also follow rules specified in the ".gitignore" file
      > if it exists in the same directory from which it is run.

--- instrumentation.ts
+++ instrumentation.ts
@@ -1,12 +1,15 @@
 import * as Sentry from '@sentry/nextjs';
 
 export async function register() {
-  if (process.env.NEXT_RUNTIME === 'nodejs') {
-    await import('./sentry.server.config');
-  }
-
-  if (process.env.NEXT_RUNTIME === 'edge') {
-    await import('./sentry.edge.config');
+  if (
+    process.env.NEXT_RUNTIME === 'nodejs' ||
+    process.env.NEXT_RUNTIME === 'edge'
+  ) {
+    Sentry.init({
+      dsn: process.env.SENTRY_DSN_URL,
+      tracesSampleRate: 1,
+      debug: false,
+    });
   }
 }
 
--- next.config.js
+++ next.config.js
@@ -47,15 +47,16 @@ module.exports = withSentryConfig(module.exports, {
   // Hides source maps from generated client bundles
   hideSourceMaps: true,
 
-  // Automatically tree-shake Sentry logger statements to reduce bundle size
-  disableLogger: true,
+  webpack: {
+    // Automatically tree-shake Sentry logger statements to reduce bundle size
+    treeshake: {
+      removeDebugLogging: true,
+    },
 
-  // Enables automatic instrumentation of Vercel Cron Monitors. (Does not yet work with App Router route handlers.)
-  // See the following for more information:
-  // https://docs.sentry.io/product/crons/
-  // https://vercel.com/docs/cron-jobs
-  automaticVercelMonitors: true,
-  sourcemaps: {
-    deleteSourcemapsAfterUpload: true, //delete sourcemaps after upload
+    // Enables automatic instrumentation of Vercel Cron Monitors. (Does not yet work with App Router route handlers.)
+    // See the following for more information:
+    // https://docs.sentry.io/product/crons/
+    // https://vercel.com/docs/cron-jobs
+    automaticVercelMonitors: true,
   },
 });
