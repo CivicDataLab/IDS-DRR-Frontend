70977e1d6698889553320f994b60f44bcde0ca58
James McKinney <26463+jpmckinney@users.noreply.github.com>
2026-04-28 20:48:44 -0400

    chore: Simplify IntlMessages declaration

--- types/global.d.ts
+++ types/global.d.ts
@@ -1,7 +1,5 @@
-// Use type safe message keys with `next-intl`
-type Messages = typeof import('../locales/en.json');
-declare interface IntlMessages extends Messages {}
+type IntlMessages = typeof import('../locales/en.json');
 
-// Types derived from IntlMessages are in global.d.ts for simplicity.
+// Types derived from Messages are in global.d.ts for simplicity.
 type RiskLevel = keyof IntlMessages['analytics']['risk'];
 type NavLinkKey = keyof IntlMessages['nav']['links'];
